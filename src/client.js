51f35baf088317f5d01cde67bc634bb34f0067d7
Aizen Baidya <aizenbaidya@outlook.com>
2025-06-21 18:02:33 -0700

    Mini fix

--- src/client.js
+++ src/client.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const URL = "https://xyceujjzyhijmrkzlbsm.supabase.co"
-// https://vite.dev/guide/env-and-mode#env-variables
+const URL = import.meta.env.VITE_SUPABASE_URL
 const API_KEY = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(URL, API_KEY);
